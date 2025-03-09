e9d0cda509c121086fbc41b14a84b381f299ec08
Rahul Bhargava <rahulbot@gmail.com>
2025-03-09 13:58:22 -0500

    try to fix base path for GH Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/city-explorer-demo/',
   plugins: [svelte()],
   build: {
     outDir: 'docs'
